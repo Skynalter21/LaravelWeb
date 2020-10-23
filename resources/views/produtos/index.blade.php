971603ecbb03f989cd84f3d9a0204605c3663e51
skynalter21 <skynalter21@gmail.com>
2020-10-23 04:47:49 -0300

    erro resolvido

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -4,7 +4,7 @@
 <?php 
 if(!isset($id)){
   $id = ""; 
-  
+
 }
 
 ?>
@@ -57,8 +57,8 @@ if(!isset($id)){
  -->
 </script>
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
+
+
 </button>
 
 <!-- Modal -->
